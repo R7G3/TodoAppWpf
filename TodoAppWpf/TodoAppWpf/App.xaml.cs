43f0b0239b0f776f961be908337c681e5d91d0f2
Вадим RAGE <tulahas@yandex.ru>
2021-06-20 15:24:42 +0300

    setup a project for MVVM

--- TodoAppWpf/TodoAppWpf/App.xaml.cs
+++ TodoAppWpf/TodoAppWpf/App.xaml.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Threading;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
+using TodoAppWpf.ViewModel;
 
 namespace TodoAppWpf
 {
@@ -13,5 +18,31 @@ namespace TodoAppWpf
     /// </summary>
     public partial class App : Application
     {
+        private ISimpleIoc container;
+
+        private void ApplicationStartup(object sender, StartupEventArgs args)
+        {
+            container = SimpleIoc.Default;
+            ServiceLocator.SetLocatorProvider(() => container);
+            DispatcherHelper.Initialize();
+
+            DispatcherUnhandledException += UnhandledExceptionHandler;
+
+            container.Register<MainViewModel>();
+
+            var window = new MainWindow()
+            {
+                DataContext = container.GetInstance<MainViewModel>()
+            };
+            container.Register<MainWindow>(() => window);
+
+            window.Show();
+        }
+
+        void UnhandledExceptionHandler(object sender, DispatcherUnhandledExceptionEventArgs args)
+        {
+            //TODO need implement
+            //try save data and show error window?
+        }
     }
 }
