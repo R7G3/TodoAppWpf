577fd4462419b9b76783cf6b72c7c44590592550
Вадим RAGE <tulahas@yandex.ru>
2021-06-21 22:42:19 +0300

    added view-model for confirmation dialog

---
+++ TodoAppWpf/TodoAppWpf/ViewModel/ConfirmationViewModel.cs
@@ -0,0 +1,39 @@
+﻿using TodoAppWpf.Helper;
+using TodoAppWpf.Properties;
+
+namespace TodoAppWpf.ViewModel
+{
+    public class ConfirmationViewModel : DialogViewModel
+    {
+        public ConfirmationViewModel()
+        {
+#if DEBUG
+            if (IsInDesignMode)
+            {
+                Title = VisualDesignerDataGenerator.LoremIpsum.Substring(0, 15);
+                ActionDescription = VisualDesignerDataGenerator.LoremIpsum.Substring(0, 200);
+            }
+#endif
+            Title = Resources.UI_ConfirmAction; 
+            ActionDescription = Resources.UI_AreYouSureWantDeleteThis;
+        }
+
+        public string Title { get; set; }
+
+        public string ActionDescription { get; set; }
+
+        public void Reset()
+        {
+            Title = Resources.UI_ConfirmAction;
+            ActionDescription = Resources.UI_AreYouSureWantDeleteThis;
+        }
+
+        public override void Cleanup()
+        {
+            Title = null;
+            ActionDescription = null;
+
+            base.Cleanup();
+        }
+    }
+}
