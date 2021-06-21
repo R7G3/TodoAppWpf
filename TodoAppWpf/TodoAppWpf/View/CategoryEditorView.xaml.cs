a15b173cabcf56349dddf5eb92aaa6e340e6cc2c
Вадим RAGE <tulahas@yandex.ru>
2021-06-21 22:32:36 +0300

    added view for category editor

---
+++ TodoAppWpf/TodoAppWpf/View/CategoryEditorView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using TodoAppWpf.Helpers;
+
+namespace TodoAppWpf.View
+{
+    /// <summary>
+    /// Логика взаимодействия для CategoryEditorView.xaml
+    /// </summary>
+    public partial class CategoryEditorView : Window
+    {
+        CloseWindowHelper helper;
+
+        public CategoryEditorView()
+        {
+            InitializeComponent();
+            helper = new CloseWindowHelper(this);
+        }
+    }
+}
