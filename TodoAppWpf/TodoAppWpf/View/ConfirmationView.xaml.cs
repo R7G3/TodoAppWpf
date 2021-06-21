ecf14a697bdcd50ac78767f7b5f4abe5f95707a7
Вадим RAGE <tulahas@yandex.ru>
2021-06-21 22:52:14 +0300

    added view for confirmation dialog

---
+++ TodoAppWpf/TodoAppWpf/View/ConfirmationView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using TodoAppWpf.Helper;
+
+namespace TodoAppWpf.View
+{
+    /// <summary>
+    /// Логика взаимодействия для ConfirmationView.xaml
+    /// </summary>
+    public partial class ConfirmationView : Window
+    {
+        CloseWindowHelper helper;
+
+        public ConfirmationView()
+        {
+            InitializeComponent();
+            helper = new CloseWindowHelper(this);
+        }
+    }
+}
