26d62473654af213cb97b07828e20ec7f8723527
Вадим RAGE <tulahas@yandex.ru>
2021-06-21 22:32:09 +0300

    added view-model for category editor

---
+++ TodoAppWpf/TodoAppWpf/ViewModel/CategoryEditorViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TodoAppWpf.Helper;
+using TodoAppWpf.Model;
+
+namespace TodoAppWpf.ViewModel
+{
+    public class CategoryEditorViewModel : DialogViewModel
+    {
+        public CategoryEditorViewModel()
+        {
+            ExistentCategories = new List<Category>();
+#if DEBUG
+            if (IsInDesignMode)
+            {
+                Category = VisualDesignerDataGenerator.GenerateCategory();
+                return;
+            }
+#endif
+
+            Category = new Category();
+        }
+
+        public Category Category
+        {
+            get;
+            set;
+        }
+
+        public ICollection<Category> ExistentCategories
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get => Category?.Name;
+            set
+            {
+                if (Category.Name == value)
+                    return;
+
+                Category.Name = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public void Reset()
+        {
+            Category = new Category();
+            ExistentCategories?.Clear();
+        }
+
+        public override void Cleanup()
+        {
+            Category = null;
+            ExistentCategories = null;
+
+            base.Cleanup();
+        }
+
+        protected override bool CanClose(bool result)
+        {
+            return result ? IsFilledAndUnique(Category) : true;
+        }
+
+        bool IsFilledAndUnique(Category category)
+        {
+            return
+                category != null
+                && !string.IsNullOrWhiteSpace(category.Name)
+                && !ExistentCategories.Any(x => x.Name == category.Name);
+        }
+    }
+}
