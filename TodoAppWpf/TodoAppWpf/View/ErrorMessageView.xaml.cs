99cde61b249ee45a3dd8adfb3ee9c400e1e11a6b
Вадим RAGE <tulahas@yandex.ru>
2021-06-21 23:09:50 +0300

    added view for error message window

---
+++ TodoAppWpf/TodoAppWpf/View/ErrorMessageView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using TodoAppWpf.Helper;
+
+namespace TodoAppWpf.View
+{
+    /// <summary>
+    /// Логика взаимодействия для ErrorMessageView.xaml
+    /// </summary>
+    public partial class ErrorMessageView : Window
+    {
+        CloseWindowHelper helper;
+
+        public ErrorMessageView()
+        {
+            InitializeComponent();
+            helper = new CloseWindowHelper(this);
+        }
+    }
+}
