35d35b46bbb1e95bfbea5e32157335de65acca6f
Вадим RAGE <tulahas@yandex.ru>
2021-06-21 21:43:17 +0300

    added base ViewModel for dialog windows

---
+++ TodoAppWpf/TodoAppWpf/ViewModel/DialogViewModel.cs
@@ -0,0 +1,53 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using GalaSoft.MvvmLight.Messaging;
+using System.Windows.Input;
+
+namespace TodoAppWpf.ViewModel
+{
+    public class DialogViewModel : ViewModelBase
+    {
+        public DialogViewModel()
+        {
+            OkCommand = new RelayCommand(
+                () => Close(true),
+                () => CanClose(true)
+            );
+
+            CancelCommand = new RelayCommand(
+                () => Close(false),
+                () => CanClose(false)
+            );
+        }
+
+        public ICommand OkCommand
+        {
+            get;
+            private set;
+        }
+
+        public ICommand CancelCommand
+        {
+            get;
+            private set;
+        }
+
+        public override void Cleanup()
+        {
+            OkCommand = null;
+            CancelCommand = null;
+
+            base.Cleanup();
+        }
+
+        protected virtual void Close(bool result)
+        {
+            MessengerInstance.Send(new NotificationMessage<bool>(this, result, Notifications.CloseDialog));
+        }
+
+        protected virtual bool CanClose(bool result)
+        {
+            return true;
+        }
+    }
+}
