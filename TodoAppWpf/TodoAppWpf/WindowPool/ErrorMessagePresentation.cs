933c5f39abb9d361338812fef236f0af14c7dce0
Вадим RAGE <tulahas@yandex.ru>
2021-06-22 11:29:54 +0300

    added error message presentation

---
+++ TodoAppWpf/TodoAppWpf/WindowPool/ErrorMessagePresentation.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using TodoAppWpf.View;
+using TodoAppWpf.ViewModel;
+
+namespace TodoAppWpf.WindowPool
+{
+    class ErrorMessagePresentation : DialogPresentation, IGenericDialogPresentation<IErrorMessage>
+    {
+        IErrorMessage content;
+
+        public IErrorMessage Content
+        {
+            get
+            {
+                return content ?? (content = new ErrorMessageViewModel());
+            }
+
+            set => content = value;
+        }
+
+
+        protected override Window CreatePresentation()
+        {
+            return new ErrorMessageView()
+            {
+                DataContext = Content,
+                Owner = MainWindow
+            };
+        }
+    }
+}
