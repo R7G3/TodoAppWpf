d796f0b8fa3b43ba4586008ca5b788b2ca00b31a
Вадим RAGE <tulahas@yandex.ru>
2021-06-22 11:25:56 +0300

    added abstract dialog presentation

---
+++ TodoAppWpf/TodoAppWpf/WindowPool/DialogPresentation.cs
@@ -0,0 +1,19 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using System.Windows;
+
+namespace TodoAppWpf.WindowPool
+{
+    abstract class DialogPresentation : IDialogPresentation
+    {
+        protected Window MainWindow = SimpleIoc.Default.GetInstance<MainWindow>();
+
+        protected abstract Window CreatePresentation();
+
+        public bool Show()
+        {
+            var window = CreatePresentation();
+            var dialogResult = window.ShowDialog();
+            return dialogResult ?? false;
+        }
+    }
+}
