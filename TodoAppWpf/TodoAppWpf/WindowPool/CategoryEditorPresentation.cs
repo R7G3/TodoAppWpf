395ca34446229ecc7c8338a52756ab7d70a510ec
Вадим RAGE <tulahas@yandex.ru>
2021-06-22 11:28:42 +0300

    added category editor presentation

---
+++ TodoAppWpf/TodoAppWpf/WindowPool/CategoryEditorPresentation.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using TodoAppWpf.View;
+using TodoAppWpf.ViewModel;
+
+namespace TodoAppWpf.WindowPool
+{
+    class CategoryEditorPresentation : DialogPresentation, IGenericDialogPresentation<ICategoryEditor>
+    {
+        ICategoryEditor content;
+
+        public ICategoryEditor Content
+        {
+            get => content ?? (content = new CategoryEditorViewModel());
+            set => content = value;
+        }
+
+
+        protected override Window CreatePresentation()
+        {
+            return new CategoryEditorView()
+            {
+                DataContext = Content,
+                Owner = MainWindow
+            };
+        }
+    }
+}
