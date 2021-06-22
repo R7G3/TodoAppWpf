b7df586f65d48fca75448ff18dc5abff8e795466
Вадим RAGE <tulahas@yandex.ru>
2021-06-22 11:29:13 +0300

    added confirmation dialog presentation

---
+++ TodoAppWpf/TodoAppWpf/WindowPool/ConfirmationDialogPresentation.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using TodoAppWpf.View;
+using TodoAppWpf.ViewModel;
+
+namespace TodoAppWpf.WindowPool
+{
+    class ConfirmationDialogPresentation : DialogPresentation, IGenericDialogPresentation<IConfirmationDialog>
+    {
+        IConfirmationDialog content;
+
+        public IConfirmationDialog Content
+        {
+            get => content ?? (content = new ConfirmationViewModel());
+            set => content = value;
+        }
+
+
+        protected override Window CreatePresentation()
+        {
+            return new ConfirmationView()
+            {
+                DataContext = Content,
+                Owner = MainWindow
+            };
+        }
+    }
+}
