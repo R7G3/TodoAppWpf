77eaed15206051154cf4f323eac30405042a5e41
Вадим RAGE <tulahas@yandex.ru>
2021-06-22 11:28:23 +0300

    added todo editor presentation

---
+++ TodoAppWpf/TodoAppWpf/WindowPool/TodoEditorPresentation.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using TodoAppWpf.View;
+using TodoAppWpf.ViewModel;
+
+namespace TodoAppWpf.WindowPool
+{
+    class TodoEditorPresentation : DialogPresentation, IGenericDialogPresentation<ITodoEditor>
+    {
+        ITodoEditor content;
+
+        public ITodoEditor Content
+        {
+            get => content ?? (content = new TodoEditorViewModel());
+            set => content = value;
+        }
+
+
+        protected override Window CreatePresentation()
+        {
+            return new TodoEditorView()
+            {
+                DataContext = Content,
+                Owner = MainWindow
+            };
+        }
+    }
+}
